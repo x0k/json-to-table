cb71df9c82e649f1c44fa32d3db3eb9b8b269030
Krasilnikov Roman <x0kill@ya.ru>
2024-08-01 01:36:51 +0300

    Bump deps

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    mk.url = "github:x0k/mk";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      mk,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            mk.packages.${system}.default
+            # pkgs.nodejs
+            pkgs.bun
+          ];
+        };
+      };
+    };
+}
