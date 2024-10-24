9ea5e9425963348fbb619ec4b0437679177e7893
Krasilnikov Roman <x0kill@ya.ru>
2024-10-24 17:40:44 +0300

    Remove old code

--- src/init.ts
+++
@@ -1,8 +0,0 @@
-import { makeURIComponentCompressor } from "@/lib/string-compressor";
-
-import { RemoteAppWorker } from './app-worker';
-
-export const compressor = makeURIComponentCompressor();
-
-export const appWorker = new RemoteAppWorker();
-appWorker.start()
--- src/lib/browser.ts
+++
@@ -1,5 +0,0 @@
-export function createPage(content: string) {
-  const win = window.open('')
-  win?.document.write(content)
-  return win
-}
