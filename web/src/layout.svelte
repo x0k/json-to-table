d403675234b6dfbcd3f3fc96be46d3f51ac056d2
Krasilnikov Roman <x0kill@ya.ru>
2024-10-24 17:37:55 +0300

    Move GitHub button

--- web/src/layout.svelte
+++ web/src/layout.svelte
@@ -5,6 +5,6 @@
 </script>
 
 <div class="flex flex-col gap-4 p-8 max-w-6xl mx-auto">
-  <div class="flex items-center"><h1 class="text-3xl font-bold grow">JSON to Table</h1>{@render append?.()}</div>
+  <div class="flex items-center gap-4"><h1 class="text-3xl font-bold grow">JSON to Table</h1>{@render append?.()}</div>
   {@render children()}
 </div>
